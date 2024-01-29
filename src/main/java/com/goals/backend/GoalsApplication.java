7e40fe7620274d97a77d99773a4e6caebddf7db2
DenilsonSilvaMonteiroFilho <49108665+DenilsonSilvaMonteiroFilho@users.noreply.github.com>
2024-01-29 07:12:20 -0300

    configuracoes iniciais para o banco e para geracao de tabelas conforme a codificacao

---
+++ src/main/java/com/goals/backend/GoalsApplication.java
@@ -0,0 +1,13 @@
+package com.goals.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalsApplication.class, args);
+	}
+
+}
